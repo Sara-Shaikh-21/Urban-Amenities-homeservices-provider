33ca4e53276259de9dcc5f9143a8b3fd4af0954d
Sara Shaikh <65680776+Sara-Shaikh-21@users.noreply.github.com>
2020-11-18 21:50:51 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php include_once "./include/header.php"; ?>
+
+<div class="container">
+    <div class="row">
+        <img src="images/demo.png" class="col-4 card-img-top" alt="Logo">
+
+        <div class="container col-8" style="margin-top: 50px;">
+            <div class="card">
+                <div class="card-header text-center">
+                    <strong>Homely Service Provider</strong>
+                </div>
+
+                <div class="card-body">
+                    The homely service provider platform helps customers book reliable home services like beauty services, massage therapy, cleaning, plumbing, carpentry, appliance repair, painting etc.We offer home maintenance, repair services, home beauty and wellness services.
+            </div>
+
+            <div class="card-footer text-center">
+                Project developed by : Sara Saeed Shaikh
+                <br>
+                Roll No : 184021
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include_once "./include/footer.php"; ?>
---
+++ indexmain.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="en">
+<link rel="stylesheet" type="text/css" href="css/style1.css">
+
+<?php
+
+include_once "./include/header.php";
+?>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<!-- partial:index.partial.html -->
+
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Neucha' rel='stylesheet' type='text/css'>
+<div id="wrapper">
+   
+   	 <h3> Homely Service Provider</h3>
+        
+   	  <div id="slider-wrap">
+      	  <ul id="slider">
+          	 <li data-color="#1abc9c">
+                <div>
+                  <img src="images/pic1.jpg">
+                  </div>                
+              </li>
+              <li data-color="#3498db">
+                <div>
+                    <img src="images/pic2.jpg">
+                </div>
+              </li>
+              <li data-color="#000000">
+                <div>
+                    <img src="images/pic3.jpg">
+                </div>
+             </li>
+             
+             <li data-color="#34495e">
+                <div>
+                    <img src="images/pic4.jpg">
+                </div>
+              </li>
+             
+             <li data-color="#e74c3c">
+                <div>
+                    <img src="images/pic5.jpg">
+                </div>
+              </li>
+
+              <li data-color="#800000">
+                <div>
+                    <img src="images/pic6.jpg">
+                </div>
+              </li>
+             
+             
+          </ul>
+          
+           <!--controls-->
+          <div class="btns" id="next"><i class="fa fa-arrow-right"></i></div>
+          <div class="btns" id="previous"><i class="fa fa-arrow-left"></i></div>
+          <div id="counter"></div>
+          
+          <div id="pagination-wrap">
+            <ul>
+            </ul>
+          </div>
+          <!--controls-->  
+                 
+      </div>
+  
+   </div>
+   
+   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+   <script type="text/javascript" src="js/js.js"></script>
+   <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+</body>
+<h2>djksssssssssssssssssssssssssssssssasbsxmjbbcsjacsjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjhxb sssssssssssssssssssssssssssssssssssssssssssssssssssss
+</h2>
+<?php
+
+include_once "./include/footer.php";
+?>
+
+</body>
+</html>
+
